0cdf9f8964022330a75159ce06f7cc2f5ab64814
Neale Upstone <neale@nealeupstone.com>
2013-03-01 14:54:22 +0000

    Remove broken implementations of setDigits/setDecimalPlaces(no args!)

--- core/src/main/java/org/osaf/cosmo/eim/DecimalField.java
+++ core/src/main/java/org/osaf/cosmo/eim/DecimalField.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 Open Source Applications Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,8 +26,8 @@ import org.apache.commons.logging.LogFactory;
 public class DecimalField extends EimRecordField {
     private static final Log log = LogFactory.getLog(DecimalField.class);
 
-    private int digits;
-    private int decimalPlaces;
+    private final int digits;
+    private final int decimalPlaces;
 
     /** */
     public DecimalField(String name,
@@ -58,17 +58,7 @@ public class DecimalField extends EimRecordField {
     }
 
     /** */
-    public void setDigits() {
-        this.digits = digits;
-    }
-
-    /** */
     public int getDecimalPlaces() {
         return decimalPlaces;
-    }
-
-    /** */
-    public void setDecimalPlaces() {
-        this.decimalPlaces = decimalPlaces;
     }
 }
