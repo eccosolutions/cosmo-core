705c7d74b60f46a44091b79ccce190e39e209568
Neale Upstone <neale@nealeupstone.com>
2021-01-14 12:47:36 +0000

    Change shippable JDK from 8 to 11

--- core/src/main/java/org/osaf/cosmo/model/hibernate/HibXmlAttribute.java
+++ core/src/main/java/org/osaf/cosmo/model/hibernate/HibXmlAttribute.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Open Source Applications Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,20 +15,16 @@
  */
 package org.osaf.cosmo.model.hibernate;
 
-import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.annotations.Type;
-import org.osaf.cosmo.model.Attribute;
-import org.osaf.cosmo.model.Item;
-import org.osaf.cosmo.model.ModelValidationException;
-import org.osaf.cosmo.model.QName;
-import org.osaf.cosmo.model.XmlAttribute;
+import org.osaf.cosmo.model.*;
 import org.w3c.dom.Element;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import java.util.Objects;
 
 /**
  * Hibernate persistent XMLAttribute.
@@ -79,6 +75,19 @@ public class HibXmlAttribute extends HibAttribute
         if (value != null && ! (value instanceof Element))
             throw new ModelValidationException("attempted to set non-Element value");
         setValue((Element) value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HibXmlAttribute that = (HibXmlAttribute) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
     /**
