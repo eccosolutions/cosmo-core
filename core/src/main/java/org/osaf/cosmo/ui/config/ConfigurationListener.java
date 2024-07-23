395a0d53c33e0856f838634b175eec6c09e5a6af
Neale Upstone <neale@nealeupstone.com>
2024-07-23 22:46:17 +0100

    Switch to jakarta Servlet API 5.0.0

--- core/src/main/java/org/osaf/cosmo/ui/config/ConfigurationListener.java
+++ core/src/main/java/org/osaf/cosmo/ui/config/ConfigurationListener.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2005-2006 Open Source Applications Foundation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,9 @@
  */
 package org.osaf.cosmo.ui.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,7 +25,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
- * A {@link javax.servlet.ServletContextListener} that uses a
+ * A {@link jakarta.servlet.ServletContextListener} that uses a
  * {@link ServletContextConfigurer} to load the Cosmo application
  * configuration when the servlet context is started.
  */
