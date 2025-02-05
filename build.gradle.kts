0672265577c68406e1b5176909eb933b87f5ef33
Neale Upstone <neale@nealeupstone.com>
2025-02-05 13:55:10 +0000

    Add maven-publish for Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `maven-publish`
+}
 
-group = "org.osaf.cosmo"
+// required by jitpack: https://docs.jitpack.io/building/
+group = "com.github.eccosolutions" // was group = "org.osaf.cosmo"
 version = "3.0.0.M1-SNAPSHOT"
