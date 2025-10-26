43199ba21eaa7e83fb290c45154318938669fc63
Adam Hamer <adam@eccosolutions.co.uk>
2025-10-26 15:31:33 +0000

    [Spring Boot] Update 3.0.x -> 3.5.9

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,7 @@ ext["hibernate.version"] = "6.4.10.Final"
 
 plugins {
     `java-library`
-    id("org.springframework.boot") version "3.0.13"
+    id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
